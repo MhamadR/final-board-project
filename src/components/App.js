69114ef3de46bcafe5fe51f979d2f739fecaabf7
MhamadR <mhamadraslan@gmail.com>
2022-12-14 20:50:10 +0200

    Establish main routers as per #6

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
+import { Route, Switch, Redirect } from "react-router-dom";
 import Boards from "./Boards";
 import TopNav from "./TopNav";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <TopNav />
-      <Boards />
+      <Switch>
+        <Route exact path="/boards">
+          <Boards />
+        </Route>
+        <Route exact path="/about"></Route>
+        <Route exact path="/completed-Tasks"></Route>
+        <Route exact path="/">
+          <Redirect to="/boards" />
+        </Route>
+      </Switch>
     </div>
   );
 }
